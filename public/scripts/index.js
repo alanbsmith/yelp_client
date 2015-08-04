3a4163b3dba22355a0e2540df99445a1f484150f
Alan Smith <a.bax.smith@gmail.com>
2015-08-04 13:13:19 -0600

    adds map toggle view functionality

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,26 +1,26 @@
 var Navbar = React.createClass({
   render: function() {
     return (
-        <nav className="navbar navbar-inverse navbar-fixed-top">
-          <div className="container">
-            <div className="navbar-header">
-              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span className="sr-only">Toggle navigation</span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-              </button>
-              <a className="navbar-brand">Yelp Client</a>
-            </div>
-            <div id="navbar" className="collapse navbar-collapse">
-              <ul className="nav navbar-nav">
-                <li><a href="https://github.com/alanbsmith/yelp_client" target="blank">view the repo</a></li>
-                <li><a href="https://github.com/alanbsmith/rails_yelp_api" target="blank">view the rails api</a></li>
-                <li><a href="https://facebook.github.io/react/index.html" target="blank">get started with react</a></li>
-              </ul>
-            </div>
+      <nav className="navbar navbar-inverse navbar-fixed-top">
+        <div className="container">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand">Yelp Client</a>
           </div>
-        </nav>
+          <div id="navbar" className="collapse navbar-collapse">
+            <ul className="nav navbar-nav">
+              <li><a href="https://github.com/alanbsmith/yelp_client" target="blank">view the repo</a></li>
+              <li><a href="https://github.com/alanbsmith/rails_yelp_api" target="blank">view the rails api</a></li>
+              <li><a href="https://facebook.github.io/react/index.html" target="blank">get started with react</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     )
   }
 });
@@ -49,54 +49,96 @@ var Search = React.createClass({
 
 var SuggestionPanels = React.createClass({
   render: function() {
-    var suggestions = this.props.info.map(function(suggestion,index) {
-      return (
-        <div className="panel">
-          <div className="panel-heading" role="tab" id="headingOne">
-            <h1 className="panel-title">
-              <a role="button" data-toggle="collapse" href={"#collapse" + index} aria-expanded="true" aria-controls="collapseOne">
-                {suggestion.name}
-              </a>
-              <p className='pull-right'>{suggestion.rating}</p>
-            </h1>
-          </div>
-          <div id={"collapse" + index} className="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
-            <div className="panel-body">
-              <strong>CONTACT INFO</strong>
-              <p>{suggestion.display_phone}</p>
-              <p>{suggestion.location.display_address['0']}</p>
-              <p>{suggestion.location.display_address['1']}</p>
-              <strong>WHAT PEOPLE ARE SAYING</strong>
-              <p>"{suggestion.snippet_text}"</p>
-              <strong><a href={suggestion.url} target='blank' >READ MORE</a></strong>
+    var businesses = this.props.info.businesses;
+    if(this.props.display === false && businesses !== undefined) {
+      var suggestions = businesses.map(function(suggestion,index) {
+        return (
+          <div className="panel">
+            <div className="panel-heading" role="tab" id="headingOne">
+              <h1 className="panel-title">
+                <a role="button" data-toggle="collapse" href={"#collapse" + index} aria-expanded="true" aria-controls="collapseOne">
+                  {suggestion.name}
+                </a>
+                <p className='pull-right'>{suggestion.rating}</p>
+              </h1>
+            </div>
+            <div id={"collapse" + index} className="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+              <div className="panel-body">
+                <strong>CONTACT INFO</strong>
+                <p>{suggestion.display_phone}</p>
+                <p>{suggestion.location.display_address['0']}</p>
+                <p>{suggestion.location.display_address['1']}</p>
+                <strong>WHAT PEOPLE ARE SAYING</strong>
+                <p>"{suggestion.snippet_text}"</p>
+                <strong><a href={suggestion.url} target='blank' >READ MORE</a></strong>
+              </div>
             </div>
           </div>
+        );
+      });
+      return (
+        <div className="suggestionPanels">
+          {suggestions}
         </div>
       );
-    });
-    return (
-      <div className="suggestionPanels">
-        {suggestions}
-      </div>
-    );
+    } else {
+      return false;
+    }
   }
 });
 
 var ResultCount = React.createClass({
   render: function() {
-    if(this.props.amount === undefined || this.props.amount === 0) {
+    if(this.props.data.businesses === undefined) {
       return false;
     } else {
+      return (<h4 id='result-count'>{this.props.data.businesses.length} results found!</h4>)
+    }
+  }
+});
+
+var MapToggle = React.createClass({
+  handleClick: function(event) {
+    this.props.onClick({map: !this.props.mapView});
+  },
+  render: function() {
+    var text = this.props.mapView ? 'List' : 'Map';
+    if (this.props.data.businesses !== undefined) {
       return (
-        <h4 id='result-count'>{this.props.amount} results found!</h4>
-      )
+        <button className="btn btn-danger pull-right" onClick={this.handleClick}>
+          {text} view
+        </button>
+      );
+    } else { return false; }
+  }
+});
+
+var MapView = React.createClass({
+  getPoints: function() {
+    var businesses = this.props.info.businesses;
+    var points = businesses.map(function(business,index) {
+        return({ latitude: business.location.coordinate.latitude,longitude: business.location.coordinate.longitude })
+    });
+    return points;
+  },
+  render: function() {
+    var region = this.props.info.region
+    if(this.props.display === true && region !== undefined) {
+      var points = this.getPoints();
+      var map = <Map latitude={region.center.latitude} longitude={region.center.longitude} zoom={13} width={1100} height={500} points={points} />;
+      console.log('mah points: ', points)
+      return (
+        <div id='map'><Map latitude={region.center.latitude} longitude={region.center.longitude} zoom={13} width={1100} height={500} points={points} />;</div>
+        );
+    } else {
+      return(false);
     }
   }
 });
 
 var Content = React.createClass({
   getInitialState: function() {
-    return {data: []};
+    return {data: [], map: false, points: []};
   },
   handleQuerySubmit: function(data) {
     var query = this.state.data;
@@ -106,13 +148,16 @@ var Content = React.createClass({
         type: 'get',
         dataType: 'json',
         success: function(data) {
-          this.setState({data: data['businesses']});
+          this.setState({data: data});
         }.bind(this),
         error: function(xhr, status, err) {
           console.error(this.props.url, status, err.toString());
         }.bind(this)
       });
     });
+  },
+  toggleMap: function(data) {
+    this.setState({map: data.map});
   },
   render: function() {
     return(
@@ -121,11 +166,13 @@ var Content = React.createClass({
         <div className='jumbotron'>
           <div className='col-md-8 col-md-offset-2'>      
             <Search onSubmit={this.handleQuerySubmit}/>
-            <ResultCount amount={this.state.data.length}/>
+            <ResultCount data={this.state.data}/>
+          <MapToggle data={this.state.data} onClick={this.toggleMap} mapView={this.state.map}/>
           </div>
         </div>
         <div className='container'>
-          <SuggestionPanels info={this.state.data}/>
+          <SuggestionPanels display={this.state.map} info={this.state.data}/>
+          <MapView display={this.state.map} info={this.state.data} />
         </div>
       </div>
     )
---
+++ public/scripts/map.js
@@ -0,0 +1,128 @@
+/**
+ * @jsx React.DOM
+ */
+"use strict";
+
+var __in_node = (typeof exports !== 'undefined' && this.exports !== exports);
+
+if( __in_node ) {
+  var React = require('react');
+}
+
+var Map = React.createClass({
+
+  // initialize local variables
+  getInitialState: function() {
+    return {
+      map : null,
+      markers : []
+    };
+  },
+
+  // set some default values
+  getDefaultProps: function() {
+    return {
+      latitude: 0,
+      longitude: 0,
+      zoom: 4,
+      width: 500,
+      height: 500,
+      points: [],
+      gmaps_api_key: '',
+      gmaps_sensor: false
+    }
+  },
+
+  // update geo-encoded markers
+  updateMarkers : function(points) {
+
+    var markers = this.state.markers;
+    var map = this.state.map;
+    
+    // map may not be loaded when parent component re-renders
+    if(map === null) { return false; }
+    
+    // remove everything
+    markers.forEach( function(marker) {
+      marker.setMap(null);
+    } );
+
+    this.state.markers = [];
+
+    // add new markers
+    points.forEach( (function( point ) {
+
+      var location = new google.maps.LatLng( point.latitude , point.longitude );
+
+      var marker = new google.maps.Marker({
+        position: location,
+        map: map,
+        title: point.label
+      });
+
+      markers.push( marker );
+
+    }) );
+
+    this.setState( { markers : markers });
+  },
+
+  render : function() {
+
+    var style = {
+      width: this.props.width,
+      height: this.props.height
+    }
+
+    return (
+      React.DOM.div({style:style})
+    );
+  },
+
+  componentDidMount : function() {
+    var createMap = (function() {
+      var mapOptions = {
+        zoom: this.props.zoom,
+        center: new google.maps.LatLng( this.props.latitude , this.props.longitude ),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+
+      var map = new google.maps.Map( this.getDOMNode(), mapOptions);
+
+      this.setState( { map : map } );
+      this.updateMarkers(this.props.points);
+    }).bind(this);
+
+    if (typeof google !== 'undefined') {
+      // scripts already loaded, create map immediately
+      createMap()
+    } else {
+      if (!window.reactMapCallback) {
+        // if this is the first time, load the scripts:
+        var s =document.createElement('script');
+        s.src = 'https://maps.googleapis.com/maps/api/js?key=' + this.props.gmaps_api_key + '&sensor=' + this.props.gmaps_sensor + '&callback=reactMapCallback';
+        document.head.appendChild( s );
+
+        // when the script has loaded, run all the callbacks
+        window.reactMapCallbacks = []
+        window.reactMapCallback = function(){
+          while (window.reactMapCallbacks.length > 0)
+            (window.reactMapCallbacks.shift())() // remove from front
+        }
+      }
+
+      // add a callback to the end of the chain
+      window.reactMapCallbacks.push(createMap)
+    }
+  },
+
+  // update markers if needed
+  componentWillReceiveProps : function(props) {
+    if( props.points ) this.updateMarkers(props.points);
+  }
+
+});
+
+if( __in_node ) {
+  module.exports = Map;
+}
